d79980b65e4fdecb4d711308e607df80e600679f
nabrious0 <162843960+nabrious0@users.noreply.github.com>
2025-11-22 15:07:51 +0100

    force build test

--- app/components/ui/Card.tsx
+++ app/components/ui/Card.tsx
@@ -8,6 +8,7 @@ interface CardProps {
 const Card = ({ children, className: customClasses }: CardProps) => {
 	return (
 		<>
+			<div>wow</div>
 			<div className={`bg-gradient-to-t from-brown-50 to-white rounded-xl shadow-sm shadow-brown-500/60 ring ring-brown-800/30 ${customClasses}`}>{children}</div>
 		</>
 	);
